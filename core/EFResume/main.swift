b4e359781c5fe3d3a1bde61800ef84cf0d182df4
Joneze <zenghui@cenghuideMacBook-Pro.local>
2017-09-19 20:55:15 +0800

    完善页面

--- core/EFResume/main.swift
+++ core/EFResume/main.swift
@@ -43,7 +43,7 @@ func input(templete: inout EFTemplete) {
     templete.basePath.set("https://joneze.github.io/myResume/")
 
     // 页面属性
-    templete.pageTitle.set("普通的简历")
+    templete.pageTitle.set("我的简历")
     templete.pageDescription.set("曾辉 的个人简历")
 
     // 基本信息
@@ -74,7 +74,7 @@ func input(templete: inout EFTemplete) {
             "目前就职于杭州聚米金融信息服务有限公司，主要负责\("聚米众筹".a(link: "https://itunes.apple.com/cn/app/聚米众筹/id1189019945?mt=8"))的开发、维护及小组管理工作，同时也负责\("影视头条".a(link: "https://itunes.apple.com/cn/app/影视头条/id932324391?mt=8") )的日常维护",
             "自 2014 年开始接触 iOS 开发，至今已有 3 年时间，熟练掌握 Objective-C 代码的编写，最近在尝试使用Swift来编写项目。熟悉大部分 iOS 开发与调试工具，理解 iOS App 结构与运行机制，注重代码质量与执行效率。",
             "了解常见移动 App 架构，长期使用 Objective-C 进行开发，熟悉各类常用第三方库的使用，目前正在尝试使用swift进行项目开发。",
-            "熟悉 iOS 的开发与发布，了解怎样利用 CocoaPods 进行打包与集成，业余时间热爱编写开源代码。",
+            "熟悉 iOS 的开发与发布，了解怎样利用 CocoaPods 进行打包与集成。",
             "熟悉 Git Flow 工作流程，有较好的 Git 使用习惯。有良好的代码风格与清晰的文档结构，遵循团队开发规范。",
             "当前主要学习 Swift 语言，也会写一些简单的前端静态页面。",
             "寻觅一份 iOS 开发的全职正式工作。".strong()
@@ -84,7 +84,7 @@ func input(templete: inout EFTemplete) {
     // 技能专长
     templete.specialities.set(
         [
-            "Swift": 30,
+            "Swift": 20,
             "Objective-C": 70
         ]
     )
@@ -93,11 +93,12 @@ func input(templete: inout EFTemplete) {
     templete.skillsList.set(
         [
             "iOS": 7,
-            "Swift": 1,
+            "Swift": 2,
             "Objective-C": 7,
             "HTML & CSS": 3,
             "Javascript": 3,
-            "Markdown": 3
+            "Markdown": 3,
+            "Photoshop":5
         ]
     )
     templete.skillsDescription.set(
@@ -108,7 +109,6 @@ func input(templete: inout EFTemplete) {
             "积攒了丰富的调试经验，",
             "热衷于用代码解决问题，",
             "熟练使用版本控制工具，",
-            "倾力于开源与技术分享，",
             "关注科技行业前沿技术。"
         ]
     )
@@ -126,12 +126,12 @@ func input(templete: inout EFTemplete) {
         [
             "3": [
                 "3 年 iOS 开发经验；",
-                "了解移动开发原理；",
+                "了解移动开发原理;",
                 "热衷 iOS 应用开发。"
             ],
             "4": [
                 "4 工作经验；",
-                "长期进行各种开发；",
+                "长期进行各种开发;",
                 "熟悉各类开发工具调试的使用。"
             ]
         ]
@@ -141,37 +141,26 @@ func input(templete: inout EFTemplete) {
     templete.project.set(
         [
             [
-                "category": "聚米众筹（2016.9 — 至今）",
+                "category": "杭州聚米金融信息服务有限公司（2015.9 — 至今）",
                 "pro_name": "iOS 团队开发和小组管理经验：聚米众筹",
                 "pro_desc": [
-                    "从 1.0.0 上架 AppStore 开始参与开发和维护工作至今，主要完成了新模块的开发、部分旧模块的重构等工作；",
-                    "作为聚米众筹 iOS 端小组负责人参与日常技术方案选型、业务迭代排期、线上异常监控、BUG 修复等工作；",
-                    "维护物流 iOS 开发组 GitHub 组织，同时维护开发组技术博客。"
+                    "从聚米APP 1.0.0 上架 AppStore 开发和维护工作至今，从0开始，主要完成了新模块的开发、部分旧模块的重构、以及线上优化bug等工作；",
+                    "作为聚米众筹 iOS 端小组负责人参与日常技术方案选型、业务迭代排期、线上异常监控、BUG 修复等工作；"
                 ]
             ],
             [
-                "category": "上海极塔客信息科技有限公司（2016.05 — 2016.12）",
-                "pro_name": "iOS 独立开发经验：玛沙多拉-动漫周边代购商城，玛沙娘-表情包",
+                "category": "杭州仙豆科技有限公司（2014.05 — 2015.9）",
+                "pro_name": "iOS 独立开发经验：瞬间-分享夜生活的吃喝玩乐社区APP，投否-面向风险投资用户，提供最前端优质创业团队信息服务APP",
                 "pro_desc": [
-                    "使用 Swift 2.3 完成整个 App 从无到有的开发、上架以及后续的升级维护工作，基本实现了一个电商 App 功能；",
-                    "研究 iOS 10 新特性，尝试开发了 iOS 表情包 App 并向公司提出建议，然后上架。"
+                    "使用 Objective-C 完成整个 App 从无到有的开发、上架以及后续的升级维护工作，基本实现了一个信息分享 App 功能；"
                 ]
             ],
             [
-                "category": "无锡乐骐科技有限公司（2015.03 — 2016.05）",
-                "pro_name": "iOS 维护和重构经验：智能证件照，KeepCap-人物相机，多来美发",
+                "category": "杭州乐卓商贸有限公司（2013.09 — 2014.02）",
+                "pro_name": "从事阿里巴巴国际站、速卖通、eBay国际电商业务员工作。",
                 "pro_desc": [
-                    "从加入公司开始学习 iOS 开发，积累了一定的 iOS 开发和维护经验；",
-                    "利用 Swift 2.3 独立对原有 App 进行了重构并完成上架。"
-                ]
-            ],
-            [
-                "category": "无锡力捷丰科技有限公司（2014.09 — 2015.01）",
-                "pro_name": "VC 业务迭代和维护经验：力捷丰高速编程器/烧录器 PC 控制软件，力捷丰科技官方网站",
-                "pro_desc": [
-                    "利用 VS 等开发工具维护基于 MFC 的上位机控制软件；",
-                    "利用 PS 等工具制作公司宣传单和其他材料；",
-                    "利用 WordPress 搭建公司网站。"
+                    "主要负责日常业务订单的处理、售前、售后的邮件回复",
+                    "利用业余时间接触了iOS开发从此一发不可收拾。确定了未来职业发展方向 "
                 ]
             ]
         ]
@@ -181,10 +170,10 @@ func input(templete: inout EFTemplete) {
     templete.openSource.set(
         [
             [
-                "name": "二维码",
-                "title": "EFQRCode（1800+ Star）",
-                "description": "EFQRCode 是一个轻量级的、用来生成和识别二维码的纯 Swift 库，可根据输入的水印图和图标产生艺术二维码，基于 CoreImage 进行开发。EFQRCode 为你提供了一种更好的在你的 App 中操作二维码的方式。",
-                "link": "https://github.com/EyreFree/EFQRCode"
+                "name": "Banner",
+                "title": "ZHScrollView",
+                "description": "ZHScrollView 是一个轻量级的、用来制作Banner 库，支持本地、网络图片设置，低耦合，使用简单。",
+                "link": "https://github.com/Joneze/ZHScrollView"
             ]
         ]
     )
@@ -192,9 +181,9 @@ func input(templete: inout EFTemplete) {
     // 扩展技能
     templete.interests.set(
         [
-            "翻译": "fi-pencil",
+            "摄影": "fi-camera",
             "前端": "fi-html5",
-            "设计": "fi-paint-bucket",
+            "PS": "fi-paint-bucket",
             "桌面": "fi-folder"
         ]
     )
